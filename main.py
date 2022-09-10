b58265cf3f56ce34b6c619e7f604d4366be38e0f
Уваров Илья <popodoss@mail.ru>
2022-09-10 15:22:23 +0300

    dz http

--- main.py
+++ main.py
@@ -1,52 +1,83 @@
+#задание 1
 import requests
 import json
 import pprint
 respauns = requests.get("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
 hero_name = [{'name':'Hulk'}, {'name': 'Captain America'}, {'name': 'Thanos'}]
-best_intelekt_name = []
-
 def best():
+    best_intelekt_name = []
     for here in respauns.json():
         for name in hero_name:
             if here['name'] == name['name']:
-                best_intelekt_name = here['powerstats']['intelligence']
-                print(best_intelekt_name)
-
+                best_intelekt = here['powerstats']['intelligence']
+                hero_int = (name['name'],best_intelekt)
+                best_intelekt_name.append(hero_int)
+    return(f'самым умным героем является {sorted(best_intelekt_name, reverse=True)}')
 print(best())
-
+# Задание 2
+token = ""
 
 # class YaUploader:
 #     def __init__(self, token: str):
 #         self.token = token
 #
-#     def upload(self, file_path: str):
+#     def get_headers(self):
+#         return {
+#             'Content-Type': 'application/json',
+#             'Authorization': f'OAuth {self.token}'
+#         }
 #
-#         self.file_path = file
-#         # Функция может ничего не возвращать
+#     def get_files_list(self):
+#         files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
+#         headers = self.get_headers()
+#         response = requests.get(files_url, headers=headers).json()
+#         return response.json()
 #
-
+#     def upload(self, disk_file_path):
+#         fail_url = "http://cloud-api.yandex.net/v1/disk/resources/upload"
+#         headers = self.get_headers()
+#         params = {'rath': disk_file_path, 'owerwrite': 'true'}
+#         response = requests.get(fail_url, headers=headers, params=params)
+#         pprint.pp(response.json())
+#         return response.json()
+#
+#
+#     def upload_file_to_disk(self, disk_file_path, filename):
+#         href = self.upload(disk_file_path=disk_file_path).get("href", "")
+#         response = requests.put(href, data=open(filename, 'rb'))
+#         response.raise_for_status()
+#         if response.status_code == 201:
+#             print("Success")
+#
 # if __name__ == '__main__':
-#     path_to_file = ...
-#     token = ...
-#     uploader = YaUploader(token)
-#     result = uploader.upload(path_to_file)
+#
+#
+#     ya = YaUploader(token=token)
+#     ya.upload_file_to_disk("disk", "fails.txt.txt")
+#     pprint.pp(ya.get_files_list())
+# Задание 2
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+                    'Content-Type': 'application/json',
+                    'Authorization': f'OAuth {self.token}'
+                }
+
+    def upload(self, file_path: str):
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        fail_url = "http://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {'rath': file_path, 'owerwrite': 'true'}
+        response = requests.get(fail_url, headers=headers, params=params)
+        return response.json()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    path_to_file = ('C:\\Users\\Илюха\\Desktop\\text.txt')
+    token = ""
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file)
+    print(uploader.get_headers())

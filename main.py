e2ad9f48bc97a65a7e36131574dbe177e40dba45
Уваров Илья <popodoss@mail.ru>
2022-09-22 01:39:10 +0300

    dorabotka dz

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 #задание 1
+TOKEN =""
 import requests
 import json
 import pprint
-# respauns = requests.get("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
-# hero_name = [{'name':'Hulk'}, {'name': 'Captain America'}, {'name': 'Thanos'}]
-# def best():
-#     best_intelekt_name = []
-#     for here in respauns.json():
-#         for name in hero_name:
-#             if here['name'] == name['name']:
-#                 best_intelekt = here['powerstats']['intelligence']
-#                 hero_int = (name['name'],best_intelekt)
-#                 best_intelekt_name.append(hero_int)
-#     return(f'самым умным героем является {sorted(best_intelekt_name, reverse=True)}')
-# print(best())
+respauns = requests.get("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
+hero_name = [{'name':'Hulk'}, {'name': 'Captain America'}, {'name': 'Thanos'}]
+def best():
+    best_intelekt_name = []
+    for here in respauns.json():
+        for name in hero_name:
+            if here['name'] == name['name']:
+                best_intelekt = here['powerstats']['intelligence']
+                hero_int = (name['name'],best_intelekt)
+                best_intelekt_name.append(hero_int)
+    return(f'самым умным героем является {sorted(best_intelekt_name, reverse=True)}')
+print(best())
 # Задание 2
 # token = ""
 
@@ -57,7 +58,7 @@ import pprint
 #     pprint.pp(ya.get_files_list())
 # Задание 2
 class YaUploader:
-    def __init__(self, token: str):
+    def __init__(self, token):
         self.token = token
 
     def get_headers(self):
@@ -66,17 +67,16 @@ class YaUploader:
                     'Authorization': f'OAuth {self.token}'
                 }
 
-    def upload(self, file_path: str):
+    def upload_fails(self, file_path: str):
 
         fail_url = "https://cloud-api.yandex.net//v1//disk//resources//upload"
         headers = self.get_headers()
         params = {'rath': file_path, 'owerwrite': 'true'}
         response = requests.get(fail_url, headers=headers, params=params)
-
         return response.json()
 
     def upload_file_to_disk(self, file_path, filename):
-        href = self.upload(file_path=file_path).get("href", "")
+        href = self.upload_fails(file_path=file_path).get("href", "")
         response = requests.put(href, data=open(filename, 'rb'))
         response.raise_for_status()
         if response.status_code == 201:
@@ -84,7 +84,6 @@ class YaUploader:
 
 if __name__ == '__main__':
     path_to_file = ('C://Users//Илюха//PycharmProjects//http//http//fails.txt')
-    token = "y0_AgAAAABkQMeUAADLWwAAAADNsbZWSaN8cJTJSCe1NnKUopgdAFbz8nQ"
-    yu = YaUploader(token)
-    result = yu.upload(path_to_file)
+    yu = YaUploader(token=TOKEN)
+    result = yu.upload_file_to_disk("dz", 'fails.txt')
     pprint.pp(result)
